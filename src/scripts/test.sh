a15c6986665822ec518c997f6c7d8ca80a039d31
Dan Caseley <dan@caseley.me.uk>
2024-04-14 19:39:51 +0100

    Add test step with example on usage

---
+++ src/scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get variables from the environment
+VERSION=$(circleci env subst "${PARAM_VERSION}")
+IP_ADDRESS=$(circleci env subst "${PARAM_IP_ADDRESS}")
+DOMAIN=$(circleci env subst "${PARAM_DOMAIN}")
+TIMEOUT=$(circleci env subst "${PARAM_TIMEOUT}")
+ADMIN_ACCOUNT_USERNAME=$(circleci env subst "${PARAM_ADMIN_ACCOUNT_USERNAME}")
+ADMIN_ACCOUNT_PASSWORD=$(circleci env subst "${PARAM_ADMIN_ACCOUNT_PASSWORD}")
+DISABLED_TESTS=$(circleci env subst "${PARAM_DISABLED_TESTS}")
+
+# Download the JAR file
+curl -L "https://github.com/XMPP-Interop-Testing/smack-sint-server-extensions/releases/download/v$VERSION/smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar" -o "smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar"
+
+echo "$IP_ADDRESS $DOMAIN" | sudo tee -a /etc/hosts
+
+java \
+    -Dsinttest.service="$DOMAIN" \
+    -Dsinttest.securityMode=disabled \
+    -Dsinttest.replyTimeout="$TIMEOUT" \
+    -Dsinttest.adminAccountUsername="$ADMIN_ACCOUNT_USERNAME" \
+    -Dsinttest.adminAccountPassword="$ADMIN_ACCOUNT_PASSWORD" \
+    -Dsinttest.enabledConnections=tcp \
+    -Dsinttest.dnsResolver=javax \
+    -Dsinttest.disabledTests="-Dsinttest.disabledTests=$DISABLED_TESTS" \
+    -jar "smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar"
