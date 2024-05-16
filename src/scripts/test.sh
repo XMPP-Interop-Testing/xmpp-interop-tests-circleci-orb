2b1f5184f873f3fd262e3a5e14134d3d65c091d0
Guus der Kinderen <guus.der.kinderen@gmail.com>
2024-05-16 21:52:41 +0200

    Enable logging of test data
    This enabled additional logging functionality as added in smack-sint-server-extensions versions 1.1 and 1.2

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -10,6 +10,7 @@ ADMIN_ACCOUNT_USERNAME=$(circleci env subst "${PARAM_ADMIN_ACCOUNT_USERNAME}")
 ADMIN_ACCOUNT_PASSWORD=$(circleci env subst "${PARAM_ADMIN_ACCOUNT_PASSWORD}")
 DISABLED_SPECIFICATIONS=$(circleci env subst "${PARAM_DISABLED_SPECIFICATIONS}")
 DISABLED_TESTS=$(circleci env subst "${PARAM_DISABLED_TESTS}")
+LOG_DIR=$(circleci env subst "${PARAM_LOG_DIR}")
 
 # Download the JAR file
 curl -L "https://github.com/XMPP-Interop-Testing/smack-sint-server-extensions/releases/download/v$VERSION/smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar" -o "smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar"
@@ -26,4 +27,7 @@ java \
     -Dsinttest.dnsResolver=javax \
     -Dsinttest.disabledSpecifications="$DISABLED_SPECIFICATIONS"  \
     -Dsinttest.disabledTests="$DISABLED_TESTS" \
+    -Dsinttest.testRunResultProcessors=org.igniterealtime.smack.inttest.SmackIntegrationTestFramework\$JulTestRunResultProcessor,org.igniterealtime.smack.inttest.util.JUnitXmlTestRunResultProcessor \
+    -Dsinttest.debugger="org.igniterealtime.smack.inttest.util.FileLoggerFactory" \
+    -DlogDir="$LOG_DIR" \
     -jar "smack-sint-server-extensions-$VERSION-jar-with-dependencies.jar"
